ecb62207db6ec00fc1369518e5077f1a065efa76
Tkk01 <thangwakimani22@gmail.com>
2023-08-24 09:24:57 -0700

    exits.c

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - duplicates a string
+ *@dest: final string to be copied to
+ *@src: initial string
+ *@n: total amount of characters copied
+ *Return: the joined string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a, b;
+	char *s = dest;
+
+	a = 0;
+	while (src[a] != '\0' && i < n - 1)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	if (a < n)
+	{
+		b = a;
+		while (b < n)
+		{
+			dest[b] = '\0';
+			b++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - joins two strings
+ *@dest: initial string
+ *@src: followed string
+ *@n: space to be used in bytes
+ *Return: the joined string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+	char *s = dest;
+
+	a = 0;
+	b = 0;
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	if (b < n)
+		dest[a] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr -finds string in character
+ *@s: parsed string
+ *@c: viewed character
+ *Return: points to memory area
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
